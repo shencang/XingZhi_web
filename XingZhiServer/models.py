baf811da812964aaf5593afa761929c2860b9174
shencang <2630610009@qq.com>
2019-06-24 18:07:13 +0800

    模型完善

--- XingZhiServer/models.py
+++ XingZhiServer/models.py
@@ -9,16 +9,17 @@ from django.db import models
 
 
 class Users(models.Model):
-    UserEmail = models.EmailField(max_length=100, primary_key=True)  # 电子邮件
-    Username = models.CharField(max_length=100, null=False)  # 用户名
-    UserPassword = models.CharField(max_length=100)  # 密码
-    UserPhone = models.CharField(max_length=12)  # 手机号
-    UserIdentity = models.CharField(max_length=2)  # 用户身份
-    UserAvatar = models.FileField(upload_to='media', null=False)  # 用户头像
-    UserSignature = models.CharField(max_length=100)  # 用户个性签名
+    userId = models.AutoField(primary_key=True)  # 用户ID
+    userEmail = models.EmailField(max_length=100)  # 电子邮件
+    username = models.CharField(max_length=100, null=False)  # 用户名
+    userPassword = models.CharField(max_length=100)  # 密码
+    userPhone = models.CharField(max_length=12)  # 手机号
+    userIdentity = models.CharField(max_length=2)  # 用户身份
+    userAvatar = models.FileField(upload_to='media', null=False)  # 用户头像
+    userSignature = models.CharField(max_length=100)  # 用户个性签名
 
     def __str__(self):
-        return self.UserEmail
+        return self.userEmail
 
 
 '''
@@ -27,10 +28,10 @@ class Users(models.Model):
 
 
 class Projects(models.Model):
-    projectId = models.AutoField(primary_key=True),  # 项目ID
-    projectName = models.TextField(),  # 项目名字
-    projectColorName = models.TextField(),  # 项目标题颜色
-    projectColorCode = models.IntegerField(),  # 项目颜色代码
+    projectId = models.AutoField(primary_key=True)  # 项目ID
+    projectName = models.TextField()  # 项目名字
+    projectColorName = models.TextField()  # 项目标题颜色
+    projectColorCode = models.IntegerField()  # 项目颜色代码
 
     def __self__(self):
         return self.projectId
@@ -43,20 +44,12 @@ class Projects(models.Model):
 
 class Labels(models.Model):
     labelId = models.AutoField(primary_key=True)  # 标签ID
-    labelName = models.TextField(),  # 标签名字
-    labelColorName = models.TextField(),  # 标签标题颜色
-    labelColorCode = models.IntegerField(),  # 标签颜色代码
+    labelName = models.TextField()  # 标签名字
+    labelColorName = models.TextField()  # 标签标题颜色
+    labelColorCode = models.IntegerField()  # 标签颜色代码
 
-
-'''
-任务和标签关系表
-'''
-
-
-class TaskLabels(models.Model):
-    taskLabelId = models.AutoField(primary_key=True),  # 任务标签关系表
-    taskId = models.IntegerField()  # 任务ID，外键
-    labelId = models.IntegerField()  # 标签ID，外键
+    def __self__(self):
+        return self.labelId
 
 
 '''
@@ -65,10 +58,56 @@ class TaskLabels(models.Model):
 
 
 class Tasks(models.Model):
-    taskId = models.AutoField(primary_key=True),  # 任务ID
-    taskTitles = models.TextField(),  # 任务标题
-    taskComment = models.TextField(),  # 任务评论
-    taskDueDate = models.IntegerField(),  # 任务截止时间
-    taskPriority = models.IntegerField(),  # 任务优先级
-    taskProjectID = models.IntegerField(),  # 任务所属项目ID，外键
-    taskStatus = models.IntegerField(),  # 任务状态
+    taskId = models.AutoField(primary_key=True)  # 任务ID
+    taskTitles = models.TextField()  # 任务标题
+    taskComment = models.TextField()  # 任务评论
+    taskDueDate = models.IntegerField()  # 任务截止时间
+    taskPriority = models.IntegerField()  # 任务优先级
+    taskProjectId = models.IntegerField()  # 任务所属项目ID,外键
+    taskUserId = models.IntegerField()  # 任务所属用户ID,外键
+    taskStatus = models.IntegerField()  # 任务状态
+
+    def __self__(self):
+        return self.taskId
+
+
+'''
+任务和标签关系表
+'''
+
+
+class TaskLabels(models.Model):
+    taskLabelId = models.AutoField(primary_key=True)  # 任务标签关系表ID
+    taskId = models.IntegerField()  # 任务ID，外键
+    labelId = models.IntegerField()  # 标签ID，外键
+
+    def __self__(self):
+        return self.taskLabelId
+
+
+'''
+用户项目关系表
+'''
+
+
+class UserProject(models.Model):
+    userProjectId = models.AutoField(primary_key=True)  # 用户项目关系表ID
+    userId = models.IntegerField()  # 用户ID，外键
+    projectId = models.IntegerField()  # 项目ID，外键
+
+    def __self(self):
+        return self.userProjectId
+
+
+'''
+用户标签关系表
+'''
+
+
+class UserLabels(models.Model):
+    userLabel = models.IntegerField(primary_key=True)  # 用户标签关系表ID
+    userId = models.IntegerField()  # 用户id外键
+    labelId = models.IntegerField()  # 标签id外键
+
+    def __self__(self):
+        return self.userLabel
